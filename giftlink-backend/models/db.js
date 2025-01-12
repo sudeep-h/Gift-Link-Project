5dd2dc2b41816acd323ce9a5ce4e2947dcb2b010
Sudeep4686 <sudeephipparge@gmail.com>
2025-01-12 17:20:02 +0530

    Gift routes added

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,8 +1,6 @@
-// db.js
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
-// MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
 
 let dbInstance = null;
@@ -15,14 +13,11 @@ async function connectToDatabase() {
 
     const client = new MongoClient(url);      
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+    await client.connect();
+    console.log("Connected successfully to server");
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
-    // Task 3: Return database instance
-    // {{insert code}}
+    const db = client.db(dbName);
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
